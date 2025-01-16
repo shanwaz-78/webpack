0fd4649b0d358f54e038087e75a1585dec901e13
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2025-01-16 12:29:30 +0530

    bundle files

---
+++ first-bundle/src/config.dev.js
@@ -0,0 +1,26 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  mode: "development",
+  entry: "/src/app.js",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 5001,
+    open: true,
+  },
+};
