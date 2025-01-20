3098d33873a187ba51b8ebdb6bf268a4e64e34fd
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2025-01-20 16:18:11 +0530

    intergret webpack

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import { renderFruites } from "./calculate.js";
+import './app.css'
+
+const fruits = ["Apple", "Banana", "Grapes"];
+
+(function () {
+  const a = (10 * Math.PI) / 2;
+  console.log(a);
+})();
+
+renderFruites(fruits);
---
+++ src/calculate.js
@@ -0,0 +1,5 @@
+export function renderFruites(fruites) {
+  fruites.forEach((fruit) => {
+    console.log(fruit);
+  });
+}
